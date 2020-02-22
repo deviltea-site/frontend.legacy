2b21e025a1c88b5d1ef95866584eb8b382e1ae04
DevilTea <ch870814@gmail.com>
2020-02-23 01:16:37 +0800

    Fix Home styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <main id="home" class="view-container">
-    <div>
-      <div id="first-line">Hello, I am</div>
-      <div id="second-line" class="font-bold">DevilTea</div>
-      <div id="third-line">a newbie frontend developer</div>
+    <div class="logo">
+      <div class="first-line">Hello, I am</div>
+      <div class="second-line font-bold">DevilTea</div>
+      <div class="third-line">a newbie frontend developer</div>
     </div>
   </main>
 </template>
