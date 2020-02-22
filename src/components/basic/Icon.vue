90896213a8b90885c08842fc83681129c994fe00
DevilTea <ch870814@gmail.com>
2020-02-23 01:03:51 +0800

    Update Icon.vue

--- src/components/basic/Icon.vue
+++ src/components/basic/Icon.vue
@@ -1,22 +1,28 @@
 <template>
-  <span>
-    <svg fill="currentColor" viewBox="0 0 24 24" :width="size" :height="size">
+  <span class="icon">
+    <svg
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      style="width: 100%; height: auto;"
+    >
       <path :d="path"></path>
     </svg>
   </span>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 import * as icons from '@mdi/js'
 
 @Component({})
 export default class Icon extends Vue {
-  @Prop() public name!: string
-  @Prop({ default: 16 }) public size!: string | number
-
   private get mdiName () {
-    return 'mdi' + this.name.split('-').map(word => word[0].toUpperCase() + word.slice(1)).join()
+    if (!this.$slots.default || !this.$slots.default[0] || !this.$slots.default[0].text) {
+      return ''
+    }
+    return 'mdi' + this.$slots.default[0].text.split('-').map(word => word[0].toUpperCase() + word.slice(1)).join()
   }
 
   private get path () {
@@ -24,3 +30,11 @@ export default class Icon extends Vue {
   }
 }
 </script>
+
+<style lang="scss">
+.icon {
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+}
+</style>
