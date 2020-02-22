b5e00e0c76803793f26afa49bfdb6685099b2dc3
DevilTea <ch870814@gmail.com>
2020-02-22 19:56:00 +0800

    Add vue example template

---
+++ templates/Example.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="Example"></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({})
+export default class Example extends Vue {
+
+}
+</script>
