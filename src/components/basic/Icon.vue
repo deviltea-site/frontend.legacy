accedcb65ee48d42e2044139d3006a0f0774a961
DevilTea <ch870814@gmail.com>
2020-02-22 19:44:48 +0800

    Add Basic Component: Icon

---
+++ src/components/basic/Icon.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>
+    <svg fill="currentColor" viewBox="0 0 24 24" :width="size" :height="size">
+      <path :d="path"></path>
+    </svg>
+  </span>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import * as icons from '@mdi/js'
+
+@Component({})
+export default class Icon extends Vue {
+  @Prop() public name!: string
+  @Prop({ default: 16 }) public size!: string | number
+
+  private get mdiName () {
+    return 'mdi' + this.name.split('-').map(word => word[0].toUpperCase() + word.slice(1)).join()
+  }
+
+  private get path () {
+    return (icons as { [key: string]: string })[this.mdiName]
+  }
+}
+</script>
