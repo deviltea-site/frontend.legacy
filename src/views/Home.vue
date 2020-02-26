fafc281d605b2c62df6f11d3345d06f106470999
DevilTea <ch870814@gmail.com>
2020-02-26 22:54:10 +0800

    Update animate and home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <main id="home" class="view-container">
     <div class="logo">
-      <div class="first-line">Hello, I am</div>
-      <div class="second-line font-bold">DevilTea</div>
-      <div class="third-line">a newbie frontend developer</div>
+      <div class="first-line slide-fade-top-in animated">Hello, I am</div>
+      <div class="second-line font-bold slide-fade-right-in animated">
+        DevilTea
+      </div>
+      <div class="third-line slide-fade-bottom-in animated">
+        a newbie frontend developer
+      </div>
     </div>
   </main>
 </template>
