a110ec17937663698e08a76f65cf092bf6fed4aa
DevilTea <ch870814@gmail.com>
2020-04-21 00:15:47 +0800

    Add circular progress component

---
+++ src/components/basic/CircularProgress.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="lds-ring" :style="{
+    '--color': color,
+    '--size': size,
+    '--width': width
+  }">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class CircularProgress extends Vue {
+  @Prop() private color!: string
+  @Prop() private size!: string
+  @Prop() private width!: string
+}
+</script>
