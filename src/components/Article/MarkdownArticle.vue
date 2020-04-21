bc52c59db73f12fb2768e2877b2a9aaecf40120a
DevilTea <ch870814@gmail.com>
2020-04-21 15:55:26 +0800

    Seperate Article component

---
+++ src/components/Article/MarkdownArticle.vue
@@ -0,0 +1,27 @@
+<template>
+  <article class="article-body">
+    <header>
+      <div class="feature-image-container"></div>
+      <div class="meta">
+        <div></div>
+      </div>
+    </header>
+    <section v-html="renderedHtml"></section>
+  </article>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import renderMarkdown from '@/utils/renderMarkdown'
+import { ArticleMeta } from '@/interfaces/API'
+
+@Component({})
+export default class MarkdownArticle extends Vue {
+  @Prop() private meta!: ArticleMeta
+  @Prop() private content!: string
+
+  private get renderedHtml () {
+    return renderMarkdown(this.content)
+  }
+}
+</script>
--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -7,47 +7,36 @@
     }"
   >
     <CircularProgress v-if="isLoading" color="#ccc"></CircularProgress>
-    <article v-else>
-      <header>
-        <div class="feature-image-container"></div>
-        <div class="meta">
-          <div></div>
-        </div>
-      </header>
-      <section v-html="renderedHtml"></section>
-    </article>
+    <MarkdownArticle v-else :meta="meta" :content="content"></MarkdownArticle>
   </main>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import CircularProgress from '@/components/basic/CircularProgress.vue'
+import MarkdownArticle from '@/components/Article/MarkdownArticle.vue'
 import { getArticleMeta, getArticleContent } from '@/controllers/articles'
 import { ArticleMeta } from '@/interfaces/API'
-import renderMarkdown from '@/utils/renderMarkdown'
 import head from '@/utils/head'
 import { delay } from '@/utils/util'
 
 @Component({
   components: {
-    CircularProgress
+    CircularProgress,
+    MarkdownArticle
   }
 })
 export default class Article extends Vue {
-  private markdownContent = ''
+  private content = ''
   private meta: ArticleMeta | null = null
   private isLoading = false
-
-  private get renderedHtml () {
-    return renderMarkdown(this.markdownContent)
-  }
 
   private async loadInitData () {
     this.isLoading = true
     try {
       const articleId = this.$route.params.articleId
       this.meta = await getArticleMeta(articleId)
-      this.markdownContent = await getArticleContent(articleId)
+      this.content = await getArticleContent(articleId)
     } catch (error) {
       await this.$router.replace({
         name: 'NotFound'
