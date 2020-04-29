4db0d0e2b128a6590a55ac27bb1ebf114c34ca97
DevilTea <ch870814@gmail.com>
2020-04-29 12:40:54 +0800

    Wrap ArticleListItem with <li>

--- src/components/ArticleList/ArticleListItem.vue
+++ src/components/ArticleList/ArticleListItem.vue
@@ -1,44 +1,49 @@
 <template>
-  <router-link
-    class="article-list-item"
-    :to="{ name: 'Article', params: { articleId: articleMeta.id } }"
-  >
-    <div class="thumb">
-      <LazyImage v-if="articleMeta.thumb" :src="articleMeta.thumb" />
-      <div v-else class="icon-container">
-        <DIcon name="book-open-variant"></DIcon>
-      </div>
-    </div>
-    <div class="info">
-      <div class="info__title">{{ articleMeta.title }}</div>
-      <div class="info__date">
-        <span>{{ time.text }}</span>
-        <span>{{ time.date.toLocaleString() }}</span>
-      </div>
-      <div class="info__description">{{ articleMeta.description }}</div>
-      <div class="info-meta">
-        <div class="info-meta__category">
-          <span class="label">分類：</span>
-          <router-link
-            class="link"
-            :to="{ name: 'ArticleList', query: { categories: articleMeta.category } }"
-            >{{ articleMeta.category }}</router-link
-          >
-        </div>
-        <div v-if="articleMeta.tags.length > 0" class="info-meta__tags">
-          <span class="label">標籤：</span>
-          <router-link
-            v-for="tag in articleMeta.tags"
-            :key="`article-${articleMeta.id}-tag-${tag}`"
-            class="tag"
-            :to="{ name: 'ArticleList', query: { tags: tag } }"
-          >
-            {{ tag }}
-          </router-link>
+  <li>
+    <router-link
+      class="article-list-item"
+      :to="{ name: 'Article', params: { articleId: articleMeta.id } }"
+    >
+      <div class="thumb">
+        <LazyImage v-if="articleMeta.thumb" :src="articleMeta.thumb" />
+        <div v-else class="icon-container">
+          <DIcon name="book-open-variant"></DIcon>
         </div>
       </div>
-    </div>
-  </router-link>
+      <div class="info">
+        <div class="info__title">{{ articleMeta.title }}</div>
+        <div class="info__date">
+          <span>{{ time.text }}</span>
+          <span>{{ time.date.toLocaleString() }}</span>
+        </div>
+        <div class="info__description">{{ articleMeta.description }}</div>
+        <div class="info-meta">
+          <div class="info-meta__category">
+            <span class="label">分類：</span>
+            <router-link
+              class="link"
+              :to="{
+                name: 'ArticleList',
+                query: { categories: articleMeta.category }
+              }"
+              >{{ articleMeta.category }}</router-link
+            >
+          </div>
+          <div v-if="articleMeta.tags.length > 0" class="info-meta__tags">
+            <span class="label">標籤：</span>
+            <router-link
+              v-for="tag in articleMeta.tags"
+              :key="`article-${articleMeta.id}-tag-${tag}`"
+              class="tag"
+              :to="{ name: 'ArticleList', query: { tags: tag } }"
+            >
+              {{ tag }}
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </router-link>
+  </li>
 </template>
 
 <script lang="ts">
